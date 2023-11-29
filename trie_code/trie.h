aaee74262e2d7d40762c867fb3567ffffee1481f
schadha009 <66641207+schadha009@users.noreply.github.com>
2023-11-28 21:03:06 -0500

    Update trie.h

--- trie_code/trie.h
+++ trie_code/trie.h
@@ -1,1 +1,33 @@
+#ifndef TRIE_H
+#define TRIE_H
 
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+
+class TrieNode {
+public:
+    std::unordered_map<char, TrieNode*> children;
+    int count;
+
+    TrieNode() : count(0) {}
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie();
+
+    void insert(const std::string& word);
+
+    std::pair<bool, int> search(const std::string& word);
+
+    void generateDotFile(const std::string& filename);
+
+private:
+    void generateDot(TrieNode* node, std::ofstream& dotFile, const std::string& prefix);
+};
+
+#endif // TRIE_H
